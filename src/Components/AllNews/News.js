725999bb2288d07ba47fe922079c4e2fafba9e17
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-01-06 00:16:29 +0600

    update

--- src/Components/AllNews/News.js
+++ src/Components/AllNews/News.js
@@ -1,16 +1,23 @@
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../Css/News.css';
+import img from '../../images/profile.jpg';
 
 const News = (props) => {
     const {title, body} = props.info;
     
     return (
-        <div className='news-box'>
-            <img src="" alt="" />
-            <div>
-                <h5>{title}</h5>
-                <p>{body}</p>
+        <div className='d-flex'>
+            <div className="news-box d-flex">
+                <img style={{width: '100px', marginRight: "10px", borderRadius: '50%'}} src={img} alt="" />
+                <div>
+                    <h5>{title}</h5>
+                    <p>{body}</p>
+                </div>
             </div>
+            <a href="#"><FontAwesomeIcon icon={faTimesCircle} /></a>
+            
         </div>
     );
 };
